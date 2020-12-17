88345e00b7f56627cbe9e2ffc35d3b31ee46bde1
Mario T. Lanza <mlanza@doesideas.com>
2020-12-17 14:58:42 -0500

    add criteria

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -1,42 +1,72 @@
 require(['atomic/core', 'atomic/dom', 'atomic/reactives', 'atomic/transducers', 'atomic/repos'], function(_, dom, $, t, repos){
 
-  function geeklist(id){
-    return _.fmap(fetch("https://boardgamegeek.com/xmlapi/geeklist/" + id), repos.xml, function(doc){
+  //get the registrations from the geeklist
+  function geeklist(params){
+    var registered = _.date(params.registered);
+    function promptRegistration(item){
+      return item.postdate < registered;
+    }
+    return _.fmap(fetch("https://boardgamegeek.com/xmlapi/geeklist/" + params.id + "/?comments=1"), repos.xml, function(doc){
       var title = _.just(doc, dom.sel1("title", _), dom.text),
-          username = _.just(doc, dom.sel1("username", _), dom.text),
-          numitems = _.just(doc, dom.sel1("numitems", _), dom.text, parseInt);
-      return _.fmap(_.just(doc, dom.sel("item", _), _.mapa(item, _), function(items){
+          username = _.just(doc, dom.sel1("username", _), dom.text);
+      return _.fmap(_.just(doc, dom.sel("item", _), _.mapa(_.partial(item, params), _), function(items){
         return Promise.all(items);
-      }), function(items){
-        return {title: title, username: username, numitems: numitems, items: items};
+      }), _.filtera(promptRegistration, _), function(items){
+        var voters = _.just(items, _.mapcat(_.get(_, "votes"), _), _.groupBy(_.get(_, "username"), _)),
+            contestants = _.unique(_.mapa(_.get(_, "username"), items));
+        return {
+          contestants: contestants,
+          voters: voters,
+          title: title,
+          host: username,
+          items: items
+        };
       });
     });
   }
 
-  function item(el){
+  //get the contest submission
+  function item(params, el){
     var subtype = dom.attr(el, "subtype"),
+        id = _.just(el, dom.attr(_ ,"id"), parseInt),
         objectid = _.just(el, dom.attr(_ ,"objectid"), parseInt),
         objectname = dom.attr(el, "objectname"),
         username = dom.attr(el, "username"),
         postdate = _.date(dom.attr(el, "postdate")),
-        body = dom.text(el);
-    return _.fmap(_.just(body, _.reFind(/\[thread=(\d+)\]|\[.*url=.+\/thread\/(\d+)\/.+\]/, _), _.drop(1, _), _.compact, _.first, thread), function(thread){
-      return Object.assign({
-          subtype: subtype,
-          objectid: objectid,
-          objectname: objectname,
-          username: username,
-          postdate: postdate,
-          body: body
-        }, thread);
-    });
+        body = _.just(el, dom.sel1("body", _), dom.text),
+        clamped = !!_.detect(function(el){
+          return _.includes(dom.text(el), "CLAMPED");
+        }, dom.sel("comment", el)),
+        eligibility = _.just([clamped ? "clamp" : null, objectid == params.hill ? "hill" : null], _.compact, _.toArray);
+    return _.fmap(_.just(body, _.reFind(/\[thread=(\d+)\]|\[.*url=.+\/thread\/(\d+)\/.+\]/, _), _.drop(1, _), _.compact, _.first, _.partial(thread, params, {id: id, objectid: objectid, objectname: objectname})), _.merge({
+      subtype: subtype,
+      id: id,
+      objectid: objectid,
+      objectname: objectname,
+      eligibility: eligibility,
+      username: username,
+      postdate: postdate,
+      body: body
+    }, _));
   }
 
   function voted(article){
-    return article.vote && !article.edited;
+    return article.vote //&& !article.edited;
   }
 
-  function thread(id){
+  var scored = _.get({"LIKE": 1.0, "LUMP": 0.75, "LOVE": 1.25}, _);
+
+  function loved(article){
+    return article.vote == "LOVE";
+  }
+
+  //get the votes for the submission
+  function thread(params, topic, id){
+    var start = _.date(params.start),
+        end = _.date(params.end);
+    function timelyVote(article){
+      return article.postdate > start && article.postdate < end;
+    }
     return id ? _.fmap(fetch("https://boardgamegeek.com/xmlapi2/thread?id=" + id), repos.xml, function(el){
       var subject = _.just(el, dom.sel1("subject", _), dom.text),
           articles = _.just(el, dom.sel("article", _), _.mapa(function(el){
@@ -47,28 +77,45 @@ require(['atomic/core', 'atomic/dom', 'atomic/reactives', 'atomic/transducers', 
                 body = _.just(el, dom.sel1("body", _), dom.text),
                 found = _.reFind(/^(LIKE|LOVE|LUMP)(.*\((\d+) minutes\))?/, body),
                 vote = _.nth(found, 1),
+                score = _.maybe(vote, scored),
                 minutes = _.maybe(found, _.nth(_, 3), _.blot, parseInt),
                 edited = _.eq(postdate, editdate) ? null : editdate;
             return {
               id: id,
+              subject: subject,
               username: username,
               postdate: postdate,
               editdate: editdate,
               edited: edited,
               body: body,
               vote: vote,
-              minutes: minutes
+              score: score,
+              minutes: minutes,
+              topic: topic
             };
-          }, _));
+          }, _)),
+          votes = _.filtera(_.and(timelyVote, voted), articles),
+          voters = _.unique(_.map(_.get(_, "username"), votes)),
+          plays = _.count(votes),
+          loves = _.count(_.filter(loved, articles)),
+          score = _.just(votes, _.map(_.get(_, "score"), _), _.sum),
+          earliest = _.maybe(votes, _.first, _.get(_, "postdate"));
       return {
         subject: subject,
         articles: articles,
-        votes: _.filtera(voted, articles)
+        earliest: earliest,
+        loves: loves,
+        plays: plays,
+        voters: voters,
+        votes: votes,
+        score: score
       };
     }) : null;
   }
 
-  //http://localhost:8080/?id=278904
-  _.just(location, _.get(_, "href"), _.fromQueryString, _.get(_, "id"), geeklist, _.fmap(_, _.log));
+  var params = _.fromQueryString(location.href);
+
+  //http://localhost:8080/?id=278904&hill=13&registered=2021-01-02T05:00:00.000Z&start=2021-02-01T05:00:00.000Z&end=2021-03-01T05:00:00.000Z
+  _.fmap(geeklist(params), _.log);
 
 });
