a5d2cba8041ed939601655304884297cfcf4cf73
Mario T. Lanza <mlanza@doesideas.com>
2020-12-17 20:49:55 -0500

    refine accolades

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -2,37 +2,68 @@ require(['atomic/core', 'atomic/dom', 'atomic/reactives', 'atomic/transducers', 
 
   //rankings
   var byScore = _.desc(_.get(_, "score")),
-      byPlays = _.desc(_.get(_, "plays"));
+      byPlays = _.desc(_.get(_, "plays")),
+      byPlayers = _.desc(_.comp(_.count, _.get(_, "voters"))),
+      byEarliest = _.desc(_.get(_, "earliest")),
+      byLoved = _.desc(_.get(_, "loves"));
 
-  //get the registrations from the geeklist
-  function geeklist(params){
+  //get submissions
+  function submissions(params){
+    return _.fmap(fetch("https://boardgamegeek.com/xmlapi/geeklist/" + params.id + "/?comments=1"), repos.xml);
+  }
+
+  //tabulate results
+  function tabulate(params){
     var registered = _.date(params.registered);
-    function promptRegistration(item){
+    function cutoff(item){
       return item.postdate < registered;
     }
-    return _.fmap(fetch("https://boardgamegeek.com/xmlapi/geeklist/" + params.id + "/?comments=1"), repos.xml, function(doc){
+    return _.fmap(submissions(params), function(doc){
       var title = _.just(doc, dom.sel1("title", _), dom.text),
           username = _.just(doc, dom.sel1("username", _), dom.text);
-      return _.fmap(_.just(doc, dom.sel("item", _), _.mapa(_.partial(item, params), _), _.filter(function(item){
-        return !item.disqualified;
-      }, _), function(items){
-        return Promise.all(items);
-      }), _.filtera(promptRegistration, _), function(items){
-        var voters = _.just(items, _.mapcat(_.get(_, "votes"), _), _.groupBy(_.get(_, "username"), _)),
+      return _.fmap(_.just(doc, dom.sel("item", _), _.mapa(_.partial(item, params), _), Promise.all.bind(Promise)), _.remove(_.get(_, "disqualified"), _), _.filter(cutoff, _), function(items){
+        var voters = _.just(items, _.mapcat(_.get(_, "votes"), _), _.groupBy(_.get(_, "username"), _), _.reducekv(function(memo, voter, votes){
+              return _.conj(memo, {username: voter, votes: votes});
+            }, [], _)),
             contestants = _.unique(_.mapa(_.get(_, "username"), items));
         return {
+          host: username,
+          title: title,
           contestants: contestants,
           voters: voters,
-          title: title,
-          host: username,
           items: items,
-          accolades: {
-            devotion: _.sort(byScore, byPlays, items),
-            plays: _.sort(byPlays, items)
-          }
+          accolades: accolades(items, voters, contestants)
         };
       });
     });
+  }
+
+  //rank outcomes
+  function accolades(items, voters, contestants){
+    var clamped = _.filtera(_.comp(_.includes(_, "clamp"), _.get(_, "eligibility")), items),
+        hill = _.just(items, _.filtera(_.comp(_.includes(_, "hill"), _.get(_, "eligibility")), _), entrants(3)),
+        centennial = _.filtera(_.comp(_.gte(_, 100), _.get(_, "score")), items),
+        quarters = _.filtera(_.comp(_.gte(_, 25), _.getIn(_, ["votes", "length"])), items),
+        dimes = _.filtera(_.comp(_.gte(_, 10), _.getIn(_, ["votes", "length"])), items);
+    return {
+      devotion: _.sort(byScore, items),
+      plays: _.sort(byPlays, items),
+      players: _.sort(byPlayers, items),
+      clamped: _.sort(byScore, clamped),
+      kingofthehill: _.sort(byScore, hill),
+      loved: _.sort(byLoved, items),
+      mvps: _.sort(_.desc(_.getIn(_, ["votes", "length"])), _.asc(_.getIn(_, ["votes", 0, "postdate"])), voters),
+      centennial: centennial, //?
+      quarters: _.sort(_.asc(_.comp(_.get(_, "postdate"), _.last, _.take(25, _), _.get(_, "votes"))), quarters),
+      dimes: _.sort(_.asc(_.comp(_.get(_, "postdate"), _.last, _.take(10, _), _.get(_, "votes"))), dimes)
+    }
+  }
+
+  //set minimum number of entrants for this accolade category
+  function entrants(min){
+    return function(items){
+      return items.length >= min ? items : [];
+    }
   }
 
   //get the contest submission
@@ -44,18 +75,14 @@ require(['atomic/core', 'atomic/dom', 'atomic/reactives', 'atomic/transducers', 
         username = dom.attr(el, "username"),
         postdate = _.date(dom.attr(el, "postdate")),
         body = _.just(el, dom.sel1("body", _), dom.text),
-        comments = dom.sel("comment", el),
-        clamped = !!_.detect(function(el){
-          return _.includes(dom.text(el), "CLAMPED");
-        }, comments),
-        disqualified = !!_.detect(function(el){
-          return _.includes(dom.text(el), "DISQUALIFIED");
-        }, comments),
+        comments = _.mapa(dom.text, dom.sel("comment", el)),
+        clamped = _.detect(_.includes(_, "CLAMPED"), comments),
+        disqualified = _.detect(_.includes(_, "DISQUALIFIED"), comments),
         eligibility = _.just([clamped ? "clamp" : null, objectid == params.hill ? "hill" : null], _.compact, _.toArray);
     return _.fmap(_.just(body, _.reFind(/\[thread=(\d+)\]|\[.*url=.+\/thread\/(\d+)\/.+\]/, _), _.drop(1, _), _.compact, _.first, _.partial(thread, params, {id: id, objectid: objectid, objectname: objectname})), _.merge({
       subtype: subtype,
       id: id,
-      disqualified: disqualified,
+      disqualified: !!disqualified,
       objectid: objectid,
       objectname: objectname,
       eligibility: eligibility,
@@ -66,7 +93,7 @@ require(['atomic/core', 'atomic/dom', 'atomic/reactives', 'atomic/transducers', 
   }
 
   function voted(article){
-    return article.vote //&& !article.edited;
+    return article.vote && article.edited;
   }
 
   var scored = _.get({"LIKE": 1.0, "LUMP": 0.75, "LOVE": 1.25}, _);
@@ -135,6 +162,6 @@ require(['atomic/core', 'atomic/dom', 'atomic/reactives', 'atomic/transducers', 
   var params = _.fromQueryString(location.href);
 
   //http://localhost:8080/?fake=1&id=278904&hill=13&registered=2021-01-02T05:00:00.000Z&start=2021-02-01T05:00:00.000Z&end=2021-03-01T05:00:00.000Z
-  _.fmap(geeklist(params), _.log);
+  _.fmap(tabulate(params), _.log);
 
 });
