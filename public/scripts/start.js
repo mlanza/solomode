9e41f09510540fd948c17296fa4062151a3afa28
Mario T. Lanza <mlanza@doesideas.com>
2021-10-02 11:28:03 -0400

    add scoring

--- public/scripts/start.js
+++ public/scripts/start.js
@@ -1,8 +1,10 @@
 import * as _core from "atomic/core";
 import * as _reactives from "atomic/reactives";
 import * as _transducers from "atomic/transducers";
+import * as _transients from "atomic/transients";
 import * as _repos from "atomic/repos";
-import {parse} from "https://deno.land/x/ts_xml_parser@1.0.0/mod.ts";
+import {parse} from "https://deno.land/x/ts_xml_parser/mod.ts"
+//import parse from "https://denopkg.com/nekobato/deno-xml-parser/index.ts";
 import {Html5Entities} from "https://raw.githubusercontent.com/matschik/deno_html_entities/master/mod.js";
 
 const core = Object.assign({}, _core);
@@ -11,8 +13,25 @@ const reactives = Object.assign({}, _reactives);
 const $ = core.impart(reactives, core.partly);
 const transducers = Object.assign({}, _transducers);
 const t = core.impart(transducers, core.partly)
+const transients = Object.assign({}, _transients);
+const mut = core.impart(transients, core.partly)
 const _repos_ = Object.assign({}, _repos);
 const repos = core.impart(_repos_, core.partly)
+
+
+function unfold(json){
+  return JSON.stringify(json, null, 2);
+}
+
+function unformat(text){
+  return _.just(text, _.replace(_, /\<br\/\>/g, "\n"), _.replace(_, /\<b\>|\<\/b\>|\<i\>|\<\/i\>/g, ""));
+}
+
+function minPlayingTime(text){
+  return _.maybe(text, _.reFind(/Playing Time: (\d*)(-\d*)|MINIMUM PLAYING TIME = (\d*)/i, _), _.drop(1, _), _.filter(_.isSome, _), _.first, parseInt);
+}
+
+const decode = _.pipe(Html5Entities.decode, _.trim, unformat);
 
 function request(url, options){
   return new Promise(function(resolve, reject){
@@ -33,7 +52,7 @@ function thread(id){
       const attributes = _.getIn(article, ["attributes"]);
       const children = _.getIn(article, ["children"]);
       const subject = _.just(children, _.detect(_.contains(_, "name", "subject"), _), _.get(_, "content"));
-      const body = _.just(children, _.detect(_.contains(_, "name", "body"), _), _.get(_, "content"), Html5Entities.decode, _.trim); //, _.replace(_, /\<br\/\>/g, "\n"), _.split(_, "\n"));
+      const body = _.just(children, _.detect(_.contains(_, "name", "body"), _), _.get(_, "content"), decode);
       return _.merge(attributes, {
         subject,
         body
@@ -63,9 +82,9 @@ function geeklist(id){
     const title = _.just(children, _.detect(_.contains(_, "name", "title"), _), _.get(_, "content"));
     const items = _.just(children, _.filtera(_.contains(_, "name", "item"), _), _.mapa(function item(item){
       const attributes = _.just(item, _.get(_, "attributes"));
-      const body = _.just(item, _.get(_, "children"), _.detect(_.contains(_, "name", "body"), _), _.get(_, "content"));
+      const body = _.just(item, _.get(_, "children"), _.detect(_.contains(_, "name", "body"), _), _.get(_, "content"), _.trim, unformat);
       const comments = _.just(item, _.get(_, "children"), _.filtera(_.contains(_, "name", "comment"), _), _.mapa(function(comment){
-        const body = comment.content;
+        const body = _.just(comment.content, _.trim, unformat);
         return _.merge(comment.attributes, {body});
       }, _));
       return _.merge(attributes, {body, comments});
@@ -75,7 +94,7 @@ function geeklist(id){
 }
 
 function getMeta(gl){
-  const items = _.just(gl, _.get(_, "items"), _.mapa(function(item){
+  const items = _.just(gl, _.get(_, "items"), _.map(function(item){
     if (item.objecttype == "thread") {
       const thread = _.maybe(item.objectid, parseInt);
       const thing = _.just(item.comments,
@@ -83,14 +102,18 @@ function getMeta(gl){
         _.map(_.opt(_.get(_, "body"), _.reFind(/thing=(\d*)/, _), _.nth(_, 1), parseInt), _),
         _.compact,
         _.first);
-      return {thread, thing, item};
+      const contents = _.toArray(_.cons(item.body, _.map(_.get(_, "body"), item.comments)));
+      const mpt = _.just(contents, _.map(minPlayingTime, _), _.compact, _.first);
+      return {thread, thing, mpt, contents, item};
     } else {
       const found = _.reFind(/\[thread=(\d*)\]|thread\/(\d*)\//g, item.body);
       const thread = _.maybe(found[1] || found[2], _.blot ,parseInt);
       const thing = _.maybe(item.objectid, parseInt);
-      return {thread, thing, item};
+      const contents = _.toArray(_.cons(item.body, _.mapcat(_.pipe(_.get(_, "body"), _.split(_, "\n")), item.comments)));
+      const mpt = _.just(contents, _.map(minPlayingTime, _), _.compact, _.first);
+      return {thread, thing, mpt, contents, item};
     }
-  }, _));
+  }, _), _.filtera(_.get(_, "mpt"), _));
   return _.merge(gl, {items});
 }
 
@@ -113,30 +136,62 @@ function less(path, limit){
   return _.updateIn(_, path, _.pipe(_.take(limit, _), _.toArray));
 }
 
-const gl = _.pipe(geeklist, _.fmap(_, getMeta, explode));
+const gl = _.pipe(geeklist,
+  _.fmap(_,
+    getMeta,
+    select("thread", [2554285]),
+    //less(["items"], 3),
+    explode,
+    plays));
 
-function gls(rootid){ //geeklist of geeklists, per Solomode 2021
+function gls(id){ //geeklist of geeklists, per Solomode 2021
   return _.fmap(
-    geeklist(rootid),
+    geeklist(id),
     _.pipe(_.get(_, "items"),
     _.mapa(_.pipe(_.get(_, "objectid"), gl), _)),
     Promise.all.bind(Promise));
 }
 
-function plays(gl, start, stop){ //TODO implement start/stop
-  const play = _.reFind(/^(LIKE|LOVE|LUMP)/, _);
+function plays(gl){
+  const play = _.pipe(_.reFind(/^(LIKE|LOVE|LUMP)(\n|$)/, _), _.nth(_, 1));
   const items = _.mapa(function(item){
     const articles = item.thread.articles;
     const plays = _.just(articles, _.filtera(_.pipe(_.get(_, "body"), play), _), _.mapa(function(article){
-      const recorded = _.maybe(article.body, play, _.nth(_, 1));
+      const recorded = _.maybe(article.body, play);
       return _.merge(article, {recorded});
-    }, _));
+    }, _), score);
     return _.merge(item, {plays});
   }, gl.items);
   return _.merge(gl, {items});
 }
 
+function score(plays){
+  const players = _.just(plays, _.map(_.get(_, "username"), _), _.unique);
+  return _.just(plays,
+    _.map(_.assoc(_, "points", [1]), _), //base score per play
+    _.reduce(function({players, bonus, plays}, play){
+      const player = _.get(play, "username");
+      const newblood = _.includes(players, player);
+      const _players = _.omit(players, player);
+      const _bonus = newblood && bonus > 4 ? bonus - 1 : bonus;
+      const _play = newblood ? _.update(play, "points", _.conj(_, bonus)) : play;
+      return {players: _players, bonus: _bonus, plays: _.conj(plays, _play)};
+    }, {players, bonus: 7, plays: []}, _),
+    _.get(_, "plays"));
+}
 
-//const json = await _.fmap(geeklist(289682), getMeta, explode, _.tee(_.log));
+ //TODO create article ranger
+ //TODO create score tabulator
+
+//const json = await _.fmap(geeklist(289682), getMeta, explode, unfold, _.tee(_.log));
 //const json = await _.fmap(gls(289676), _.mapcat(_.get(_, "items"), _), _.toArray, _.tee(_.log)); //Solomode 2021 line up
-const json = await _.fmap(geeklist(278904), getMeta, less(["items"], 2), explode, plays, _.tee(_.log));
+function select(what, ids){
+  return function(gl){
+    const items = _.filtera(function(item){
+      return _.includes(ids, _.get(item, what));
+    }, gl.items);
+    return _.merge(gl, {items});
+  }
+}
+//await _.fmap(thread(2554285), /*_.getIn(_, ["root", "children"]), _.mapcat(_.get(_, "children"), _), _.toArray,*/  _.tee(_.log));
+await _.fmap(gl(278904), _.get(_, "items"), _.mapa(_.get(_, "plays"), _), _.tee(_.log));
